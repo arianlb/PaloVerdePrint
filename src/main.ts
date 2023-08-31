0f33722542c15d2ccf2d07e0ad81eee067b6a6e5
arianlb <83365444+arianlb@users.noreply.github.com>
2023-08-31 14:04:05 -0400

    17 prueba de cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // Enable CORS*/
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
