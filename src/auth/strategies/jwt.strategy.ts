62346f1a6820e7abe33af338ddea264c098f1aa0
arianlb <83365444+arianlb@users.noreply.github.com>
2023-08-31 11:19:25 -0400

    7ma prueba de cors

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -11,7 +11,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor(@InjectModel(User.name) private readonly userModel: Model<User>) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: process.env.JWT_SECRET
+            secretOrKey: process.env.JWT_SECRET,
+            crossOriginIsolated: true
         });
     }
 
